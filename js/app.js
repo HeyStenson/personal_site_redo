1249075faca9ef36ef0367b97ae25dfb289da9e3
Heather Stenson <hstenson@fb.com>
2016-07-01 08:44:24 -0700

    js switching basics in place

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 console.log("hey girl");
 
+var current_link;
+
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
+
+	$(".navbar-fixed a").click(function() {
+		console.log("hey");
+		if(!$(this).hasClass("active")){
+			current_link = this;
+			$(".section:visible").fadeOut(1300, function() { 
+				$(".navbar-fixed a").removeClass("active");  
+				$(current_link).addClass("active");
+				var new_link = $($(current_link).attr("href"));
+				new_link.fadeIn(1300);
+			});
+		}
+		return false;
+	})
 });
